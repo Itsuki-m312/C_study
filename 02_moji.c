bd4db761ca438ca0c30b1dcaf8eb31b79d656de0
Itsuki-m312 <261241799+Itsuki-m312@users.noreply.github.com>
2026-03-16 13:19:02 +0900

    改行の方法

---
+++ 02_moji.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// int main (void){
+//     printf("こんにちは");
+//     printf("こんばんは");
+//     return 0;
+// }
+
+int main (void){
+    printf("こんにちは\n");
+    printf("こんばんは\n");
+    return 0;
+}
